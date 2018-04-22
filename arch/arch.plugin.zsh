fdeadfa93bb7516a95385fcd955585ee6b78a196
Pavel <picnixer@gmail.com>
2018-04-22 19:56:54 +0500

    fix arch plugin

--- arch/arch.plugin.zsh
+++ arch/arch.plugin.zsh
@@ -4,9 +4,11 @@ pacman() {
   elif [ $1 = "update" ] ; then
     sudo /usr/bin/pacman -Sy
   elif [ $1 = "upgrade" ] ; then
-    sudo /usr/bin/pacman -Syu
+    sudo /usr/bin/pacman -Suyy
   elif [ $1 = "search" ] ; then
     /usr/bin/pacman -Ss ${@:2}
+  elif [ $1 = "remove" -o $1 = "purge" ] ; then
+    sudo /usr/bin/pacman -Rs ${@:2}
   else
     sudo /usr/bin/pacman $@
   fi
@@ -18,9 +20,11 @@ pacaur() {
   elif [ $1 = "update" ] ; then
     /usr/bin/pacaur -Sy
   elif [ $1 = "upgrade" ] ; then
-    /usr/bin/pacaur -Syu
+    /usr/bin/pacaur -Suyy
   elif [ $1 = "search" ] ; then
     /usr/bin/pacaur -Ss ${@:2}
+  elif [ $1 = "remove" -o $1 = "purge" ] ; then
+    sudo /usr/bin/pacaur -Rs ${@:2}
   else
     /usr/bin/pacaur $@
   fi
